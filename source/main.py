21a77ed4cf5c69cee5a9c487a5c313458fbe12ff
Tobias Mueller <tobiasmueller@tobiass-mbp.localdomain>
2021-02-05 20:40:24 +0100

    changed code

--- source/main.py
+++ source/main.py
@@ -14,16 +14,6 @@ def run(output_dir, s3destination):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("output_dir",
-                        help="The output location to save the data to")
-
-    parser.add_argument("--s3uri",
-                        help="This is optional, provide the path if you want to upload the data to s3", default=None)
-
-    parser.add_argument("--log-level", help="Log level", default="INFO", choices={"INFO", "WARN", "DEBUG", "ERROR"})
-
-    args = parser.parse_args()
 
     # Set up logging
     logging.basicConfig(level=logging.getLevelName(args.log_level), handlers=[logging.StreamHandler(sys.stdout)],
@@ -31,8 +21,8 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     # Start process
-    logger.info("Starting run with arguments...\n{}".format(args.__dict__))
+    logger.info("Starting run with arguments...")
 
-    run(args.output_dir, args.s3uri)
+    logger.info("Hello World!")
 
     logger.info("Completed run...")
